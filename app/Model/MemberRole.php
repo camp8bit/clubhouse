1f69236112862d0ebe05c739465dd53d1d70caa6
Sam Minnee <sam@silverstripe.com>
2019-03-22 14:26:04 +1300

    Fix strict type issue

--- app/Model/MemberRole.php
+++ app/Model/MemberRole.php
@@ -4,11 +4,12 @@ namespace Clubhouse\Model;
 
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Security\Member;
+use SilverStripe\Forms\FieldList;
 
 class MemberRole extends DataExtension
 {
 
-    public function updateCMSFields($fields)
+    public function updateCMSFields(FieldList $fields)
     {
         $fields->removeByName('Locale');
         $fields->removeByName('FailedLoginCount');
